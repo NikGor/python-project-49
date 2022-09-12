439b69c0e8217d0b13006314da97fadad833d8d7
Nikolai Gordienko <nicolas.gordienko@gmail.com>
2022-09-12 19:25:19 +0200

    fixed test errors #1

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -13,7 +13,7 @@ RULES = 'Find the greatest common divisor of given numbers.'
 def get_question_and_answer():
     rand1 = random.randint(1, MAX_VALUE)
     rand2 = random.randint(1, MAX_VALUE)
-    question_and_answer = (f'{rand1}, {rand2}', str(get_gcd(rand1, rand2)))
+    question_and_answer = (f'{rand1} {rand2}', str(get_gcd(rand1, rand2)))
     return question_and_answer
 
 
