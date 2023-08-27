90a399ff24bddce13943fe19aa60398c90c3d822
Nikolai <nicolas.gordienko@gmail.com>
2023-08-27 22:13:47 +0200

    fix: fixed is_prime

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -14,7 +14,7 @@ def get_question_and_answer():
 def is_prime(number):
     if number <= 1:
         return False
-    for i in range(2, number // 2):
+    for i in range(2, (number // 2) + 1):
         if number % i == 0:
             return False
     return True
