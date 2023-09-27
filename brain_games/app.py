f7b985c7547754a3d999e83d4c4ed62e97ac1ac3
Nikolai <nicolas.gordienko@gmail.com>
2023-09-28 00:41:16 +0200

    feat: single-page-architecture

--- brain_games/app.py
+++ brain_games/app.py
@@ -1,35 +1,48 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify
 from flask_cors import CORS
 from models import db, Game
 from admin import admin, GameAdminView
 from brain_games.games import brain_calc, brain_even, brain_gcd, brain_prime, brain_progression
-
-games = {
-    "brain_calc": brain_calc,
-    "brain_even": brain_even,
-    "brain_gcd": brain_gcd,
-    "brain_prime": brain_prime,
-    "brain_progression": brain_progression,
-}
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 CORS(app)
+load_dotenv()
 
-app.config['SECRET_KEY'] = 'your-secret-key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///games.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
 
 db.init_app(app)
 admin.init_app(app)
 
-# Добавляем GameAdminView к admin здесь, чтобы избежать циклических импортов
 admin.add_view(GameAdminView(Game, session=db.session))
+
+games = {
+    "Calculator": brain_calc,
+    "Even": brain_even,
+    "GCD": brain_gcd,
+    "Prime": brain_prime,
+    "Progression": brain_progression
+}
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     games = Game.query.all()
     return render_template('index.html', games=games)
+
+
+@app.route('/api/<game_name>')
+def get_game_data(game_name):
+    rules = games[game_name].RULES
+    question, answer = games[game_name].get_question_and_answer()
+    return jsonify({
+        'rules': rules,
+        'question': question,
+        'answer': answer
+    })
 
 
 if __name__ == '__main__':
--- brain_games/models.py
+++ brain_games/models.py
@@ -6,4 +6,4 @@ db = SQLAlchemy()
 class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
-    description = db.Column(db.String(200), nullable=True)
+    description = db.Column(db.String(500), nullable=True)
